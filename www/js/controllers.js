e88575f930a1a3bc3a7accbf8e42b3e3e5a09667
mattrothenberg <mattrothenberg@gmail.com>
2015-07-31 23:44:37 -0400

    Interaction details and FAB

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -43,6 +43,7 @@ angular.module('starter.controllers', [])
 
 .controller('NavCtrl', function($scope) {
   $scope.isMenuOpen = false;
+  $scope.fabActive = false;
 
   $scope.openMenu = function() {
     $scope.isMenuOpen = true;
@@ -51,10 +52,73 @@ angular.module('starter.controllers', [])
   $scope.closeMenu = function() {
     $scope.isMenuOpen = false;
   }
+
+  $scope.toggleFab = function() {
+    if ( $scope.fabActive === true) {
+      $scope.fabActive = false;
+    } else {
+      $scope.fabActive = true;
+    }
+  }
 })
 
-.controller('InboxCtrl', function($scope) {
+.controller('InboxCtrl', function($scope, $timeout) {
+  $scope.emails = [
+    {
+      id: 0,
+      done: false,
+      subject: 'Ionic Framework',
+      subClass: '',
+      avatar: {
+        url: '../img/ionic-logo.png',
+        initial: '',
+      },
+      participants: 'Ionic Team, me',
+      body: "Yeah, it's an awesome framework! If only I actually knew what I was doing",
+      attachments: {}
+    },
+    {
+      id: 1,
+      done: false,
+      subject: 'We need to talk...',
+      subClass: 'girlfriend',
+      avatar: {
+        url: '',
+        initial: 'G'
+      },
+      participants: 'Girlfriend',
+      body: "I think you're obsessed with coding",
+      attachments: {}
+    },
+    {
+      id: 2,
+      done: false,
+      subject: 'Care Package',
+      subClass: 'mom',
+      avatar: {
+        url: '',
+        initial: 'M'
+      },
+      participants: 'Mom',
+      body: "I sent you some goodies!",
+      attachments: [
+        { name: 'Cookies.docx' },
+        { name: 'Pretzels.docx' },
+        { name: 'Rugelach.docx' },
+        { name: 'Jelly.docx' },
+      ]
+    }
+  ];
 
+  $scope.echoCurrentSlide = function(index, id) {
+    if (index == 0) {
+      var arrItem = _.where($scope.emails, {id: id});
+      var arrItemIndex = _.indexOf($scope.emails, arrItem[0]);
+      $scope.emails.splice(arrItemIndex, 1);
+    }
+  }
+
+  console.log($scope.emails);
 })
 
 .controller('PlaylistCtrl', function($scope, $stateParams) {
